1011b2d159bdb522befb086dfa3994ae2f14caed
afriliapink <afrilia04@outlook.com>
2017-09-28 11:53:58 +0700

    versi 1.0

---
+++ all.php
@@ -0,0 +1,216 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <meta charset="UTF-8">
+ <title>Menu + Leve Culinária Light</title>
+ <meta name="description" content="Menu + Leve Culinária Light é uma opção diferenciada para quem quer perder peso sem abrir mão do bom preço, do requinte e do sabor." />
+ <link rel="icon" type="image/x-icon" href="/content/images/favicon.ico" />
+ <link rel="icon" type="image/png" href="/content/images/favicon.png" />
+ <meta property="og:site_name" content="Menu + Leve"/>
+ <meta property="og:type" content="website"/>
+ <meta property="og:title" content="Menu + Leve Culinária Light"/>
+ <meta property="og:description" content="Menu + Leve Culinária Light é uma opção diferenciada para quem quer perder peso sem abrir mão do bom preço, do requinte e do sabor." />
+ <meta property="og:url" content="http://menumaisleve.com.br"/>
+ <meta property="og:image" content="http://menumaisleve.com.br/content/images/facebook.png" />
+ <link rel="stylesheet" href="/content/css/above.css"/>
+ <meta id='viewport' name="viewport" content="width=device-width, minimum-scale=0.4, maximum-scale=1.0, user-scalable=no"/>
+ <script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-55986992-1', 'auto'); ga('send', 'pageview'); </script>
+ <script>var JS = {};</script>
+ </head>
+ <body>
+ <div id="MenuMaisLeve">
+ <header id="page-header">
+ <div id="main-nav">
+ <nav class="hidden">
+ <ul class="cf_">
+ <li class="done">
+ <a href="/cardapio">
+ <span>Cardápio</span>
+ </a>
+ <div class="drop">
+ <a href="/cardapio/aves">
+ <span>Aves</span>
+ </a>
+ <a href="/cardapio/carnes">
+ <span>Carnes</span>
+ </a>
+ <a href="/cardapio/massas">
+ <span>Massas</span>
+ </a>
+ <a href="/cardapio/sobremesas">
+ <span>Sobremesas</span>
+ </a>
+ <a href="/cardapio/sopas">
+ <span>Sopas</span>
+ </a>
+ </div>
+ </li>
+ <li>
+ <a href="/onde-encontrar">
+ <span>Onde Encontrar</span>
+ </a>
+ </li>
+ <li>
+ <a href="/mais-leve">
+ <span>+ Leve</span>
+ </a>
+ </li>
+ <li>
+ <a href="/fale-conosco">
+ <span>Fale Conosco</span>
+ </a>
+ </li>
+ </ul>
+ </nav>
+ <div class="logo">
+ <a href="/">
+ <span>Menu + Leve</span>
+ </a>
+ </div>
+ </div>
+ </header>
+ <main id="page-content">
+ <div class='content current'>
+ <div id="splash" class="main-content" data-title="Menu + Leve Culinária Light">
+ <section id="splash-painel">
+ <div class="perv-carousel"><div>
+ <h3 class="tenha-um-dia hidden">
+ <span>Tenha um</span>
+ <span>
+ <i class="i-svg" data-icon="plus" data-color="FFFFFF"></i>
+ </span>
+ <span>dia</span>
+ <span>Saudável</span>
+ </h3>
+ <div class='fill' data-src='/content/images/paineis/dia-saudavel/background.jpg'></div>
+ </div>
+ </div>
+ </section>
+ <section id="splash-learn-more">
+ <div class="wrapper">
+ <div class="content cf_">
+ <div class="products">
+ <img alt="Produtos diversos" src='/content/images/splash/products.png' />
+ </div>
+ <div class="about">
+ <div class="dotted">
+ <h3>Menu + Leve é<br /> uma opção diferenciada<br /> para quem quer<br /> perder peso sem abrir mão<br /> do bom preço, do requinte<br /> e do sabor.</h3>
+ </div>
+ <div class="info">
+ <p>Apoiada em um know-how na área de<br /> culinária light, a linha de produtos é<br /> especialmente indicada para dietas especiais e<br /> para reeducação alimentar. Sempre com sabor,<br /> praticidade, qualidade e saúde.</p>
+ <a href="/sobre" class="button">
+ <span>SAIBA MAIS</span>
+ </a>
+ </div>
+ </div>
+ </div>
+ </div>
+ </section>
+ <section id="splash-cardapio" class="tiles">
+ <div class="wrapper">
+ <a href="/mais-leve/alimentacao-saudavel" class="tile saudavel">
+ <div class="wrapper">
+ <div class="cell">
+ <img alt="Alimentação saudável" src='/content/images/mais-leve/saudavel.svg' />
+ <h3>um dia de alimentação<br/>saudável</h3>
+ </div>
+ </div>
+ </a>
+ <div class="tile" id="tile-cardapio">
+ <div class="wrapper">
+ <figure>
+ <a href="/produto/penne-a-4-queijos/">
+ <img alt="Penne aos quatro queijos" data-title="Penne aos|quatro queijos" src='/content/images/splash/produtos/penne-quatro-queijos.jpg' />
+ </a>
+ <a href="/produto/strogonoff-de-carne-e-pure-de-batatas/">
+ <img alt="Strogonoff de carne" data-title="Strogonoff|de carne" src="/content/images/pixel.gif" data-src='/content/images/splash/produtos/strogonoff-de-carne.jpg' />
+ </a>
+ <a href="/produto/risoto-de-frango/">
+ <img alt="Risoto de frango" data-title="Risoto|de frango" src="/content/images/pixel.gif" data-src='/content/images/splash/produtos/risoto-de-frango.jpg' />
+ </a>
+ </figure>
+ <div class="ribbon">
+ <div class="text">
+ <strong>Penne aos<br /> quatro queijos</strong>
+ </div>
+ <i class='i-svg' data-icon='plus,,reload' data-color='93d2d7'></i>
+ </div>
+ <div class="nav cf_">
+ <span>
+ <i class='i-svg' data-icon='left' data-color='FFFFFF'></i>
+ </span>
+ <span>
+ <i class='i-svg' data-icon='right' data-color='FFFFFF'></i>
+ </span>
+ </div>
+ </div>
+ </div>
+ <a href="/mais-leve/metodo-de-conservacao" class="tile metodo">
+ <div class="wrapper">
+ <div class="cell">
+ <img alt="Método de Conservação" src='/content/images/mais-leve/metodo.svg' />
+ <h3>nosso método de<br/>conservação</h3>
+ </div>
+ </div>
+ </a>
+ </div>
+ </section>
+ <section class="bottom-map">
+ <div class="results">
+ <div class="wrapper">
+ <h4>Porto Alegre</h4>
+ <div class='close'>
+ <i class="i-svg" data-icon="close"></i>
+ </div>
+ <div class="unit">
+ </div>
+ </div>
+ </div>
+ <div class="map">
+ <img class="svg" alt='Onde Encontrar' src='/content/images/onde-encontrar/mapa.svg' />
+ </div>
+ <div class="clouds">
+ 	
+ </div>
+ <div class="where">
+ <div class="wrapper">
+ <h3>Onde<br />Encontrar</h3>
+ <p>Veja onde encontrar<br/>os produtos Menu + Leve</p>
+ <a href="/onde-encontrar" class="button">
+ <span>LOCALIZAR</span>
+ </a>
+ </div>
+ </div>
+ </section>
+ </div>
+ </div>
+ <div class="content loader"></div>
+ </main>
+ <footer id="page-footer">
+ <div class="wrapper">
+ <div class="logo">
+ <img alt="Menu + Leve logo" src='/content/images/bottom-logo.svg' />
+ </div>
+ <address>
+ <a href="mailto:contato@menumaisleve.com.br">contato@menumaisleve.com.br</a>
+ <span>(51) 99471.7958</span>
+ <em>Delivery para clientes de POA e Grande POA<br />Pagamento via cartão de crédito e débito</em>
+ </address>
+ </div>
+ <a title="Perverte" target="_blank" href="http://perverte.me" class="demonietto">Demonietto</a>
+ </footer>
+ <div id="page-loader">
+ <div class="wrapper">
+ <div class="content">
+ <div class="loader">	
+ </div>
+ </div>
+ <div class="ribbon">
+ </div>
+ </div>
+ </div>
+ </div>
+ <script> var cb = function () { var e, a, t = ["/content/css/base.css"]; a = document.getElementById("viewport"); for (var n in t) e = document.createElement("link"), e.rel = "stylesheet", e.href = t[n], a.parentNode.insertBefore(e, a) }, raf = window.addEventListener("load", cb); </script>
+ <script async data-main="/Scripts/build" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.14/require.min.js"></script>
+ </body>
+ </html>
